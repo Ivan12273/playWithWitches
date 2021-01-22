99d5a6f203915f9ff2caf196b30ac10b50a8d97f
Ivan <ivanaguilar01@live.com.mx>
2021-01-22 09:51:52 -0600

    All the project
    The functions, moves, sprites, animations and controllers.

---
+++ playWithSatan/Assets/Scripts/Prota.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prota : MonoBehaviour
+{
+    public float speed = 3f;
+
+    Animator anim;
+    Rigidbody2D rb2d;
+    Vector2 mov;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Se detecta el movimiento en un vector 2D
+        mov = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+        );
+
+        if(mov != Vector2.zero)
+        {
+            anim.SetFloat("movX", mov.x);
+            anim.SetFloat("movY", mov.y);
+            anim.SetBool("walking", true);
+        }
+        else
+        {
+            anim.SetBool("walking", false);
+        }
+   
+    }
+
+    void FixedUpdate()
+    {
+        // Nos movemos en el fixed por las físicas
+        rb2d.MovePosition(rb2d.position + mov * speed * Time.deltaTime);
+    }
+
+}
